d8eff3ac6c224d1769c149873ce7d85c228acb9c
GVetters <gavetter@ttu.edu>
2025-05-28 12:26:20 -0500

    did some rough work for pressure calculations

--- plot_weather_test.py
+++ plot_weather_test.py
@@ -3,21 +3,21 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from matplotlib.ticker import MaxNLocator
-files=["p129.118.107.232_output_205052701.csv", "p129.118.107.205_output_205052701.csv", "p129.118.107.234_output_205052701.csv", "p129.118.107.204_output_205052701.csv", "p129.118.107.233_output_205052701.csv" ,"p129.118.107.235_output_205052701.csv"]
+files=["p129.118.107.205_output_2025052701.csv", "p129.118.107.234_output_2025052701.csv", "p129.118.107.204_output_2025052701.csv", "p129.118.107.233_output_2025052701.csv", "p129.118.107.235_output_2025052701.csv" ,"p129.118.107.232_output_2025052701.csv"]
 
-naming_dict={"p129.118.107.232_output_205052701.csv": "Pi A", 
-"p129.118.107.205_output_205052701.csv": "Pi B", 
-"p129.118.107.234_output_205052701.csv": "Pi C", 
-"p129.118.107.204_output_205052701.csv": "Pi D", 
-"p129.118.107.233_output_205052701.csv": "Pi E",
-"p129.118.107.235_output_205052701.csv": "Pi F"}
+naming_dict={"p129.118.107.205_output_2025052701.csv": "Pi A", 
+"p129.118.107.234_output_2025052701.csv": "Pi B", 
+"p129.118.107.204_output_2025052701.csv": "Pi C", 
+"p129.118.107.233_output_2025052701.csv": "Pi D", 
+"p129.118.107.235_output_2025052701.csv": "Pi E",
+"p129.118.107.232_output_2025052701.csv": "Pi F"}
 
-colors = {"p129.118.107.232_output_205052701.csv": "b", 
-"p129.118.107.205_output_205052701.csv": "r", 
-"p129.118.107.234_output_205052701.csv": "g", 
-"p129.118.107.204_output_205052701.csv": "y", 
-"p129.118.107.233_output_205052701.csv": "c",
-"p129.118.107.235_output_205052701.csv": "m"}
+colors = {"p129.118.107.205_output_2025052701.csv": "b", 
+"p129.118.107.234_output_2025052701.csv": "r", 
+"p129.118.107.204_output_2025052701.csv": "g", 
+"p129.118.107.233_output_2025052701.csv": "y", 
+"p129.118.107.235_output_2025052701.csv": "c",
+"p129.118.107.232_output_2025052701.csv": "m"}
 
 
 #cut_off_date = pd.to_datetime("2025-05-27 00:00:00")
@@ -25,7 +25,7 @@ colors = {"p129.118.107.232_output_205052701.csv": "b",
 fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 
 for i in files:
-    df = pd.read_csv("/home/daq2-admin/APD-WeatherStation/data_folder"+i)
+    df = pd.read_csv("/home/daq2-admin/APD-WeatherStation/data_folder/"+i)
     df['Time'] = pd.to_datetime(df['Time'])
     time=df["Time"].to_numpy()
     humidity=df["Humidity"].to_numpy()
@@ -35,7 +35,6 @@ for i in files:
     # Apply mask to filter events after the 5th of February
     #mask = time < cut_off_date
 
-    #mask = (humidity >= 10) & (humidity <= 60)
     axs[0].scatter(time, humidity, marker='o', s=20, color=colors[i], label=naming_dict[i])
     axs[0].set_xlabel("Time")
     axs[0].set_ylabel("Humidity %%")
@@ -68,7 +67,6 @@ for i in files:
     for tick in ticks:
         tick.set_horizontalalignment('right')
 
-  
     #plt.xticks(rotation=45)
     for ax in axs:
         ax.tick_params(axis='x', rotation=30)
@@ -81,3 +79,17 @@ for i in files:
 plt.savefig("/home/daq2-admin/APD-WeatherStation/test_overlaid_plots.png")
 
 print("Plots should be finished! (God help us all)")
+#print("inside of pressure: ", pressure)
+
+df1 = pd.read_csv("/home/daq2-admin/APD-WeatherStation/data_folder/p129.118.107.235_output_2025052701.csv")
+pressure1=df1["Pressure"].to_numpy()
+avr_D = np.average(pressure1, axis=None)
+print("average pressure from Pi E is ", avr_D, "hPa")
+
+df2 = pd.read_csv("/home/daq2-admin/APD-WeatherStation/data_folder/p129.118.107.232_output_2025052701.csv")
+pressure2=df2["Pressure"].to_numpy()
+avr_F = np.average(pressure2, axis=None)
+print("average pressure from pi F is ", avr_F, "hPa")
+
+diff = avr_D - avr_F
+print("The mean difference between Pi E (Chase area) and Pi F (lobby) is ", diff, "hPa")
